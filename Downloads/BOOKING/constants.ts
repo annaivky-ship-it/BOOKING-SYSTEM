32127dc392c346053979f1500b12f3e48cf62bfa
annaivky-ship-it <annaivky@gmail.com>
2025-12-15 11:56:37 +0800

    Delete Downloads directory

--- Downloads/BOOKING/constants.ts
+++
@@ -1,7 +0,0 @@
-// constants.ts
-
-export const DEPOSIT_PERCENTAGE = 0.15; // 15%
-// PayID Configuration - Recipients can pay to either email or phone
-export const PAY_ID_NAME = 'Anna Ivky';
-export const PAY_ID_EMAIL = 'annaivky@gmail.com';
-export const PAY_ID_PHONE = '0414461008';
