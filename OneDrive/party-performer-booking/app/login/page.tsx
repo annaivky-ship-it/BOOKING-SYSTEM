3d5224b41f414387d17ea5f39f812f56ab67ed32
annaivky-ship-it <annaivky@gmail.com>
2025-12-15 11:56:24 +0800

    Delete OneDrive/party-performer-booking directory

--- OneDrive/party-performer-booking/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminLogin from '../components/AdminLogin';
-
-export default function LoginPage() {
-  return (
-    <main className="container" style={{ minHeight: '100vh', paddingTop: '2rem' }}>
-      <AdminLogin />
-    </main>
-  );
-}
